710f59d0b3016e9143d7e0e5bcfdefbeec9559bd
Ali Shahidi <alishahidi267@gmail.com>
2023-07-01 11:35:09 +0330

    add list of project method

--- app/http/controller/project.controller.js
+++ app/http/controller/project.controller.js
@@ -5,29 +5,39 @@ class ProjectController {
     async createProject(req, res, next) {
         try {
             const { title, text } = req.body;
-            
-            const image = req?.file?.path?.substring(7) 
+
+            const image = req?.file?.path?.substring(7)
             console.log(image);
             const owner = req.user._id
 
-            const result = await ProjectModel.create({ title, text, owner,image });
+            const result = await ProjectModel.create({ title, text, owner, image });
             if (!result) throw { status: 400, message: 'افزودن پروژه با مشکل مواجه شد' }
 
-            
-             return res.status(201).json({
+
+            return res.status(201).json({
                 status: 201,
                 success: true,
                 message: 'پروژه با موفقیت انجام شد'
             })
 
         } catch (error) {
-            
+
             next(error)
         }
     }
 
-    getAllProject() {
-
+    async getAllProject(req, res, next) {
+        try {
+            const owner = req.user._id
+            const projects = await ProjectModel.find({ owner })
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                projects
+            })
+        } catch (error) {
+            next(error)
+        }
     }
     getProjectById() {
 
